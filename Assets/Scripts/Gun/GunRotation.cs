79e798e36899bc950bdfa2ecad6d22d7d4460d9a
Aibulat <119883177+Sobackakot@users.noreply.github.com>
2024-02-26 18:48:26 +0300

    save

--- Assets/Scripts/Gun/GunRotation.cs
+++ Assets/Scripts/Gun/GunRotation.cs
@@ -1,23 +1,24 @@
-
-using UnityEditor.Build.Content;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+ 
+using UnityEngine; 
 
 public class GunRotation : MonoBehaviour 
 { 
-
-    public float rotationSpeed = 5f; 
+    public float rotationSpeed = 5f;
+    [SerializeField] private Camera myCamera;
     void Update()
-    { 
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         
+    {
+        //берем позицию мышки с помошью камеры
+        Vector3 mousePos = myCamera.ScreenToWorldPoint(Input.mousePosition); 
+
+        //мы получаем отрезок от точки А к точке Б и переводим в радианы и получаем угол
         float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg;
-         
         angle -= 90f;
-         
+         // вращаем точку А по заданному углу 
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-         
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        
+        //делавем вращение плавным от текущего угла к заданному углу
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); 
+       
     }
     
 }
